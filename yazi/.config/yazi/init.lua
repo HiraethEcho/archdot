1cf6ee2dec3e933003d20eeb47ddf7de19e6288f
arch24 <wyz2016zxc@outlook.com>
2024-12-20 19:29:47 +0800

    arch24 add scripts for dwm click

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -19,9 +19,9 @@ Header:children_add(function()
   return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
 end, 500, Header.LEFT)
 
-require("git"):setup({
-  show_branch = true,
-})
+-- require("git"):setup({
+--   show_branch = true,
+-- })
 
 require("session"):setup {
 	sync_yanked = true,
