fe9972542391c800b3d059962125b957d2a44a73
arch24 <wyz2016zxc@outlook.com>
2024-12-29 16:20:25 +0800

    arch24 add yazi plugins

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,17 +1,3 @@
-Status:children_add(function()
-  local h = cx.active.current.hovered
-  if h == nil or ya.target_family() ~= "unix" then
-    return ui.Line {}
-  end
-
-  return ui.Line {
-    ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
-    ui.Span(":"),
-    ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
-    ui.Span(" "),
-  }
-end, 500, Status.RIGHT)
-
 Header:children_add(function()
   if ya.target_family() ~= "unix" then
     return ui.Line {}
@@ -19,11 +5,79 @@ Header:children_add(function()
   return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
 end, 500, Header.LEFT)
 
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		" ",
+	}
+end, 500, Status.RIGHT)
+
 require("git"):setup({
   show_branch = true,
 })
 
+-- require("githead"):setup()
+
 require("session"):setup {
-	sync_yanked = true,
+  sync_yanked = true,
 }
-require("eza-preview"):setup()
+
+--[[ require("yatline"):setup({
+  show_background = false,
+
+  header_line = {
+    left = {
+      section_a = {
+        { type = "line", custom = false, name = "tabs", params = { "left" } },
+      },
+      section_b = {
+      },
+      section_c = {
+      }
+    },
+    right = {
+      section_a = {
+        { type = "string", custom = false, name = "date", params = { "%A, %d %B %Y" } },
+      },
+      section_b = {
+        { type = "string", custom = false, name = "date", params = { "%X" } },
+      },
+      section_c = {
+      }
+    }
+  },
+
+  status_line = {
+    left = {
+      section_a = {
+        { type = "string", custom = false, name = "tab_mode" },
+      },
+      section_b = {
+        { type = "string", custom = false, name = "hovered_size" },
+      },
+      section_c = {
+        { type = "string",   custom = false, name = "hovered_path" },
+        { type = "coloreds", custom = false, name = "count" },
+      }
+    },
+    right = {
+      section_a = {
+        { type = "string", custom = false, name = "cursor_position" },
+      },
+      section_b = {
+        { type = "string", custom = false, name = "cursor_percentage" },
+      },
+      section_c = {
+        { type = "string",   custom = false, name = "hovered_file_extension", params = { true } },
+        { type = "coloreds", custom = false, name = "permissions" },
+      }
+    }
+  },
+}) ]]
